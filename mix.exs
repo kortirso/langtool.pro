1d40c3c4e8f86e84992b717494911177c90f2125
kortirso <kortirso@gmail.com>
2019-09-10 14:15:23 +0300

    release 0.1.3

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule LangtoolPro.Mixfile do
   def project do
     [
       app: :langtool_pro,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.4",
       name: "Langtool.pro",
       description: @description,
